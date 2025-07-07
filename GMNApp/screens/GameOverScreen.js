85b590d869509f15426070cfde0ead2d4fbf0d1a
erol.saroglu <saroglu.erol@gmail.com>
2025-07-07 22:11:06 +0300

    screens and button component are added

---
+++ GMNApp/screens/GameOverScreen.js
@@ -0,0 +1,9 @@
+import {View} from 'react-native';
+
+function  GameOverScreen() {
+  return (
+    <View></View>
+  );
+}   
+
+export default GameOverScreen;
---
+++ GMNApp/screens/GameScreen.js
@@ -0,0 +1,9 @@
+import {View} from 'react-native';
+
+function  GameScreen() {
+  return (
+    <View></View>
+  );
+}   
+
+export default GameScreen;
