b677d836354d4bace8904876e78701fcec1b14f5
erol.saroglu <saroglu.erol@gmail.com>
2025-07-08 22:39:57 +0300

    expo-linear-gradient added, styling components

--- GMNApp/App.js
+++ GMNApp/App.js
@@ -1,17 +1,20 @@
 import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    // <View style={styles.container}>
+    <LinearGradient style={styles.container} colors={['#4e0329', '#ddb52f']}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddb52f',
   },
 });
--- GMNApp/screens/StartGameScreen.js
+++ GMNApp/screens/StartGameScreen.js
@@ -1,14 +1,64 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
     return (
-        <View>
-            <TextInput></TextInput>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false}>
+            </TextInput>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}> 
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        padding: 16,
+        backgroundColor: '#4e0329',
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    numberInput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        width: 50,
+        alignSelf: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 16,
+    },
+    buttonContainer: {
+        flex: 1,
+    },
+})                          
